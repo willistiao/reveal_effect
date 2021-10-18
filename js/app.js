cdc514046ba406ff7894d5d8fbd3ea1fe7e95a7e
Willis <tiaowillis2@gmail.com>
2021-10-18 08:05:02 -0700

    working + added new color/font

--- js/app.js
+++ js/app.js
@@ -3,7 +3,92 @@ const ctx = canvas.getContext('2d');
 const links = [...document.querySelectorAll('li')];
 
 function lerp(start, end, t){
-    return start * (1 - t) * end * t;
+    return start * (1 - t) + end * t;
+}
+
+
+
+let imgIndex = 0;
+// Load images into an array for reference
+const images = [
+    './images/1.jpg',
+    './images/2.jpg',
+    './images/3.jpg',
+    './images/4.jpg',
+    './images/5.jpg',
+    './images/6.jpg'
+]
+
+let imgArr = [];
+
+// Canvas mousemove varaibles
+
+let targetX = 0;
+let targetY = 0;
+let currentX = 0;
+let currentY = 0;
+
+window.addEventListener('mousemove', (e)=> {
+    targetX = e.clientX;
+    targetY = e.clientY;
+    
+})
+
+images.forEach((image, idx) => {
+    let elImage = new Image(300);
+    elImage.src = image;
+    elImage.classList.add('project-image');
+    document.body.append(elImage);
+    imgArr.push(elImage)
+})
+
+// Draw images to the canvas
+
+let percent = 0;
+let target = 0;
+
+function drawImage(idx){
+    let {width, height} = imgArr[idx].getBoundingClientRect();
+
+    canvas.width = width * window.devicePixelRatio;
+    canvas.height = height * window.devicePixelRatio;
+    canvas.style.width = `${width}px`;
+    canvas.style.height = `${height}px`;
+
+    // pixelate by diabling the smoothing
+    ctx.webkitImageSmoothingEnabled = false;
+    ctx.mozImageSmoothingEnabled = false;
+    ctx.msSmoothingEnabled = false;
+    ctx.imageSmoothingEnabled = false;
+
+    if(target === 1){ // Link has been hovered
+        // 2 speeds to make the effect more gradual
+        if(percent < 0.2){
+            percent += .01;
+        }else if(percent < 1){
+            percent += .1;
+        }
+    }else if(target === 0){
+        if(percent > 0.2){
+            percent -= .3
+        }else if( percent > 0){
+            percent -= .01;
+        }
+    }
+
+    let scaledWidth = width * percent;
+    let scaledHeight = height * percent;
+
+    if(percent >= 1){
+        ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+        ctx.drawImage(imgArr[idx], 0, 0, width, height);
+    }else{
+        ctx.drawImage(imgArr[idx], 0, 0, scaledWidth, scaledHeight);
+        ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+        if(canvas.width !== 0 && canvas.height !== 0){
+            ctx.drawImage(canvas, 0, 0, scaledWidth, scaledHeight, 0, 0, width, height)
+        }
+    }
 }
 
 for(let i = 0; i < links.length; i++){
@@ -12,7 +97,7 @@ for(let i = 0; i < links.length; i++){
             if(j !== i){
                 links[j].style.opacity = 0.2;
                 links[j].style.zIndex = 0;
-            } else {
+            }else{
                 links[j].style.opacity = 1;
                 links[j].style.zIndex = 3;
             }
@@ -24,48 +109,24 @@ for(let i = 0; i < links.length; i++){
             links[i].style.opacity = 1;
         }
     })
+
+    links[i].addEventListener('mouseenter', () => {
+        imgIndex = i;
+        target = 1
+    });
+
+    links[i].addEventListener('mouseleave', () => {
+        target = 0;
+    })
 }
 
-let imgIndex = 0;
-// Load Images into an array for reference
-const images = [
-    './images/1.jpg',
-    './images/2.jpg',
-    './images/3.jpg',
-    './images/4.jpg',
-    './images/5.jpg',
-    './images/6.jpg',
-]
-
-let imgArr = [];
-
-// Canvas mousemove variables
-
-let targetX = 0;
-let targetY = 0;
-let currentX = 0;
-let currentY = 0;
-
-window.addEventListener('mousemove', (e) => {
-    targetX = e.clientX;
-    targetY = e.clientY;
-})
-
-images.forEach((image, idx) => {
-    let elImage = newImage(300);
-    elImage.src = image;
-    elImage.classList.add('project-image');
-    document.body.appendChild(elImage);
-    imgArr.push(elImage)
-})
-
-// Draw images to the canvas
-
-function drawImage(idx){
-    let {width, height} = imgArr[idx].getBoundingClientRect();
-
-    // makes images clearer for all screens
-    canvas.width = width * window.devicePixelRatio; 
-    canvas.width = height * window.devicePixelRatio; 
-    
+function animate(){
+    currentX = lerp(currentX, targetX, 0.075);
+    currentY = lerp(currentY, targetY, 0.075);
+    let { width, height} = imgArr[imgIndex].getBoundingClientRect();
+    canvas.style.transform = `translate3d(${currentX - (width / 2)}px, ${currentY - (height / 2)}px, 0)`;
+    drawImage(imgIndex);
+    window.requestAnimationFrame(animate);
 }
+
+animate()
